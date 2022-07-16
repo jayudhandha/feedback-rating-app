2b442d1c1a2e14791499b81b7d2078189c158a2c
jayesh dhandha <jayudhandha@gmail.com>
2022-07-16 19:08:10 +0530

    added json-server backend with CRUD in app

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,34 +1,32 @@
-import { createContext, useState } from "react"
-import { v4 } from "uuid"
+import { createContext, useEffect, useState } from "react"
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This is feedback item 1",
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: "This is feedback item 2",
-      rating: 9,
-    },
-    {
-      id: 3,
-      text: "This is feedback item 3",
-      rating: 9,
-    },
-  ])
-
+  const [feedback, setFeedback] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
 
-  const deleteFeedback = (id) => {
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  const fetchFeedback = async () => {
+    const response = await fetch("/feedback?_sort=id")
+    const data = await response.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
+  const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
+      await fetch("/feedback/" + id, {
+        method: "DELETE",
+      })
+
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
@@ -40,19 +38,39 @@ export const FeedbackProvider = ({ children }) => {
     })
   }
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = v4()
-    console.log(newFeedback)
-    // feedback.push(newFeedback)
-    // setFeedback(feedback)
-    setFeedback([newFeedback, ...feedback])
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch("/feedback", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    })
+
+    const data = await response.json()
+
+    setFeedback([data, ...feedback])
+    // Below code for managing feedback in context instead of backend server
+    // newFeedback.id = v4()
+    // console.log(newFeedback)
+    // // feedback.push(newFeedback)
+    // // setFeedback(feedback)
+    // setFeedback([newFeedback, ...feedback])
   }
 
-  const updateFeedback = (id, updatedItem) => {
+  const updateFeedback = async (id, updatedItem) => {
+    const response = await fetch("/feedback/" + id, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedItem),
+    })
+
+    const data = await response.json()
+
     setFeedback(
-      feedback.map((item) =>
-        item.id === id ? { ...item, ...updatedItem } : item
-      )
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     )
   }
 
@@ -60,6 +78,7 @@ export const FeedbackProvider = ({ children }) => {
     <FeedbackContext.Provider
       value={{
         feedback,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
